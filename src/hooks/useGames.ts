717ba10a5ddab5aa8203b9541a03cbb1d7f98984
Elena Solovieva <esolovieva@hotmail.com>
2024-06-12 13:25:00 +0200

    Implement searching

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,5 @@
 import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenres";
 
 export interface Platform {
   id: number;
@@ -20,9 +19,14 @@ export interface Game {
 const useGames = 
 (gameQuery: GameQuery) => 
   useData<Game>("/games", 
-    {params: 
-      {genres: gameQuery.genre?.id, 
+    {
+      params: {
+        genres: gameQuery.genre?.id, 
         platforms: gameQuery.platform?.id,
-      ordering: gameQuery.sortOrder}}, [gameQuery]);
+        ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText
+      }
+    }, 
+    [gameQuery]);
 
 export default useGames;
